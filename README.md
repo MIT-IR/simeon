1eef0d53c202c98b30f3eb3e4c6aea7d440fc8a9
Abdou Seck <aseck@mit.edu>
2021-01-20 23:31:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### simeon
+
+`simeon` is a package and CLI tool to help with the processing of edx Research data
+
+### Developing
+```sh
+git clone this_project
+cd simeone
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements_dev.txt
+pip install -e .
+simeon --help  # to invoke the executable
+```
---
+++ setup.py
@@ -0,0 +1,49 @@
+import glob
+from setuptools import find_packages, setup
+
+
+setup(
+    name='simeon',
+    version='0.0.1',
+    author='MIT Institutional Research',
+    author_email='irx@mit.edu',
+    packages=find_packages(),
+    url='https://github.com/MIT-IR/simeon',
+    license='LICENSE',
+    keywords=[
+        'edx research data', 'mitx', 'edx',
+        'MOOC', 'education', 'online learning'
+    ],
+    python_requires='>=3.5',
+    description='Process research data from edX',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'simeon=simeon.scripts.simeon:main',
+        ],
+    },
+    install_requires=[
+        'boto3>=1.16.57',
+        'google-cloud-bigquery>=2.6.2',
+        'google-cloud-storage>=1.35.0',
+        'python-dateutil>=2.8.1',
+    ],
+    package_data={'simeon.schemas': ['*.json'] },
+    test_suite="simeon.tests",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Data Analysts :: Data Engineers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Data Analysis',
+    ],
+)
