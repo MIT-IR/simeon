9dd7fa1c0f2e6dd97358a844c13051f3370017f7
Abdou Seck <aseck@mit.edu>
2021-01-21 22:22:48 -0500

    Add functions to split tracking logs

--- simeon/download/logs.py
+++ simeon/download/logs.py
@@ -1,3 +1,80 @@
 """
 Module to process tracking log files from edX
 """
+import gzip
+import json
+import os
+from datetime import datetime
+from typing import Dict, List, Union
+
+from dateutil.parser import parse as parse_date
+
+import simeon.download.utilities as utils
+
+
+
+def process_line(
+    line: Union[str, bytes], lcount: int,
+    date: Union[None, datetime]=None, is_gzip=True
+) -> dict:
+    """
+    Process the line from a tracking log file and return the reformatted
+    line (deserialized) along with the name of its destination file.
+
+    :type line: Union[str, bytes]
+    :param line: A line from the tracking logs
+    :type lcount: int
+    :param lcount: The line number of the given line
+    :type date: Union[None, datetime]
+    :param date: The date of the file where this line comes from.
+    :type is_gzip: bool
+    :param is_gzip: Whether or not this line came from a GZIP file
+    :rtype: Dict[str, Union[Dict[str, str], str]]
+    :return: Dictionary with both the data and its destination file name
+    """
+    line = line.strip()
+    if isinstance(line, bytes):
+        line = line.decode('utf8', 'ignore')
+    if not line.startswith('{'):
+        if 'localhost {' in line[:27]:
+            line = line[26:]
+    try:
+        record = json.loads(line)
+    except json.decoder.JSONDecodeError:
+        return {'data': line, 'filename': 'dead_letter_queue.json'}
+    course_id = utils.get_course_id(record)
+    record['course_id'] = course_id
+    utils.rephrase_mongo_keys(record)
+    if not date:
+        try:
+            date = parse_date(record.get('time', ''))
+            outfile = utils.make_tracklog_path(course_id, date, is_gzip)
+        except Exception:
+            ext = '.gz' if is_gzip else ''
+            outfile = os.path.join(
+                course_id.replace('.', '_').replace('/', '__'),
+                'tracklog-unknown.json{x}'.format(x=ext)
+            )
+    else:
+        outfile = utils.make_tracklog_path(course_id, date, is_gzip)
+    return {'data': record, 'filename': outfile}
+
+
+def split_tracking_log(filename: str, ddir: str):
+    """
+    Split the records in the given GZIP tracking log file
+    """
+    fhandles = dict()
+    with gzip.open(filename) as zfh:
+        for i, line in enumerate(zfh):
+            line_info = process_line(line, i + 1)
+            fname = line_info.get('filename')
+            if fname not in fhandles:
+                fhandles[fname] = utils.make_file_handle(fname, is_gzip=True)
+            fhandle = fhandles[fname]
+            if isinstance(fhandle, gzip.GzipFile):
+                fhandle.write(
+                    json.dumps(line_info['data']).encode('utf8', 'ignore')
+                )
+            else:
+                json.dump(line_info['data'], fhandle)
---
+++ simeon/download/utilities.py
@@ -0,0 +1,165 @@
+"""
+Some utility functions
+"""
+import gzip
+import os
+import re
+from datetime import datetime
+from functools import lru_cache
+
+from dateutil.parser import parse as parse_date
+
+
+CID_PATT1 = re.compile('^http[s]*://[^/]+/courses/([^/]+/[^/]+/[^/]+)/', re.I)
+CID_PATT2 = re.compile('/courses/([^/]+/[^/]+/[^/]+)/', re.I)
+
+
+def make_file_handle(fname: str, mode: str='w', is_gzip: bool=False):
+    """
+    Create a file handle pointing the given file name.
+    If the directory of the file does not exist, create it.
+
+    :type fname: str
+    :param fname: A file name whose handle needs to be created.
+    :type mode: str
+    :param mode: "a[b]?" for append or "w[b]?" for write
+    :type is_gzip: bool
+    :param is_gzip: Whether or not to open it as a gzip file handle
+    :rtype: Union[TextIOWrapper, BufferedReader]
+    """
+    fname = os.path.expanduser(fname)
+    dirname, _ = os.path.split(fname)
+    if dirname:
+        os.makedirs(dirname, exist_ok=True)
+    if is_gzip:
+        return gzip.open(fname, mode)
+    return open(fname, mode)
+
+
+def get_course_id(record: dict) -> str:
+    """
+    Given a JSON record, try getting the course_id out of it.
+
+    :type record: dict
+    :param record: A deserialized JSON record
+    :rtype: str
+    :return: A valid edX course ID or an empty string
+    """
+    course = record.get('course_id', '')
+    if not course:
+        if 'browser' in record.get('event_source', '').lower():
+            match = CID_PATT1.search(record.get('page', ''))
+            if match:
+                course = match.group(1)
+        else:
+            match = CID_PATT2.search(record.get('event_type', ''))
+            if match:
+                course = match.group(1)
+    return course
+
+
+@lru_cache(maxsize=None)
+def make_tracklog_path(course_id: str, date: datetime, is_gzip=True) -> str:
+    """
+    Make a local file path name with the given course ID and datetime object
+
+    :type course_id: str
+    :param course_id: Properly formatted edX course ID
+    :type date: datetime
+    :param date: Date associated with records that would be saved in the file
+    :type is_gzip: bool
+    :param is_gzip: Whether or not we're making a GZIP file path
+    :rtype: str
+    :return: A local FS file path
+    """
+    datestr = date.strftime('%Y-%m-%d')
+    ext = '.gz' if is_gzip else ''
+    return os.path.join(
+        course_id.replace('.', '_').replace('/', '__'),
+        'tracklog-{ds}.json{x}'.format(ds=datestr, x=ext)
+    )
+
+@lru_cache(maxsize=None)
+def parse_mongo_tstamp(timestamp: str):
+    """
+    Try converting a MongoDB timestamp into a stringified datetime
+
+    :type timestamp: str
+    :param timestamp: String representing a timestamp
+    :rtype: str
+    :return: A formatted datetime
+    """
+    if not timestamp:
+        return ''
+    try:
+        timestamp = int(timestamp)
+    except (TypeError, ValueError):
+        try:
+            return str(parse_date(timestamp))
+        except Exception:
+            try:
+                return str(parse_date(timestamp[:16]))
+            except Exception:
+                msg = '{t} is not a valid timestamp'.format(t=timestamp)
+                raise ValueError(msg) from None
+    try:
+        return str(datetime.fromtimestamp(timestamp/1000.0))
+    except Exception:
+        msg = '{t} is not a valid timestamp'.format(t=timestamp)
+        raise ValueError(msg) from None
+
+
+def rephrase_mongo_keys(record: dict):
+    """
+    Update the given record in place. The target keys
+    are those whose values came from a MongoDB export
+
+    For now, I am skipping Ike's check_for_funny_keys function
+    from the ephrase_forum_data.py module in edx2bigquery
+
+    :type record: dict
+    :param record: A deserialized JSON record
+    :rtype: None
+    :return: Nothing
+    """
+    date_keys = (
+        ('endorsement', 'time'), ('updated_at', '$date'),
+        ('created_at', '$date'), ('last_activity_at', '$date')
+    )
+    str_keys = (
+        ('historical_abuse_flaggers', None),
+        ('abuse_flaggers', None),
+        ('at_position_list', None),
+        ('tags_array', None),
+        ('up', 'votes'),
+        ('up', 'votes')
+    )
+    if '_id' in record:
+        record['mongoid'] = record['_id']['$oid']
+        record.pop('_id')
+    if 'parent_id' in record:
+        data['parent_id'] = record['parent_id']['$oid']
+    for key, subkey in date_keys:
+        if key in record:
+            record[key] = parse_mongo_tstamp(record[key][subkey])
+    if 'comment_thread_id' in record:
+        record['comment_thread_id'] = record['comment_thread_id']['$oid']
+    endorsement_conds = (
+        'endorsement' in record,
+        record.get('endorsement', '') in ('null', None, ''),
+    )
+    if all(endorsement_conds):
+        record.pop('endorsement')
+    if 'parent_ids' in record:
+        record['parent_ids'] = ' '.join(
+            [subrec['$oid'] for subrec in record['parent_ids']]
+        )
+    for key, subkey in str_keys:
+        if key in record:
+            if subkey:
+                subrec = record.get(subkey, {})
+                subrec[key] = str(subrec.get(key, ''))
+            else:
+                record[key] = str(record.get(key, ''))
+    for key in ('context', 'depth', 'retired_username'):
+        record.pop(key, None)
