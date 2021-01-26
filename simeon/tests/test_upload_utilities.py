8ee2d09529a4cf4f2b86489cc96eb14b1e8e6e9a
Abdou Seck <aseck@mit.edu>
2021-01-26 08:24:09 -0500

    Add some GCP utilities

---
+++ simeon/tests/test_upload_utilities.py
@@ -0,0 +1,64 @@
+"""
+Unit tests for upload utilities
+"""
+import unittest
+
+from simeon.upload import utilities as uputils
+from simeon.exceptions import (
+    BigQueryNameException, MissingSchemaException
+)
+
+
+class TestUploadUtilities(unittest.TestCase):
+    """
+    Test the utility functions and classes in the upload package
+    """
+    def setUp(self):
+        self.good_file_mames = [
+            ('log', 'data/MITx__123__2T2020/tracklog-2020-06-01.json.gz'),
+            ('sql', 'data/MITx__123__2T2020/user_info_combo.csv.gz'),
+            ('sql', 'data/MITx__123__2T2020/person_course.csv.gz'),
+            ('sql', 'data/MITx__123__2T2020/forum.csv.gz'),
+            ('sql', 'data/MITx__123__2T2020/forum_person.csv.gz'),
+            ('email', 'data/MITx__123__2T2020/email-opt-in.csv.gz'),
+        ]
+        self.bad_file_names = [
+            ('log', 'data/MITx__123__2T2020/no-date-here.json.gz'),
+            ('sql', 'data/MITx__123__2T2020/.hidden.csv.gz')
+        ]
+        self.missing_schema_tables = [
+            'this_table_doesnt_exist_anywhere',
+            'neither_does_this',
+        ]
+        self.project = 'our-awesome-project'
+        self.bucket = 'our-awesome-bucket'
+    
+    def test_good_table_names(self):
+        """
+        Test valid BigQuery table names from file names
+        """
+        for ftype, fname in self.good_file_mames:
+            msg = 'With file name {f} and type {t}'.format(f=fname, t=ftype)
+            with self.subTest(msg):
+                out = uputils.local_to_bq_table(fname, ftype, self.project)
+                self.assertTrue(len(out.split('.')) == 3)
+    
+    def test_bad_table_names(self):
+        """
+        Test that badly named data files can't make BigQuery table names
+        """
+        for ftype, fname in self.bad_file_names:
+            msg = 'With file name {f} and type {t}'.format(f=fname, t=ftype)
+            with self.subTest(msg):
+                with self.assertRaises(BigQueryNameException):
+                    uputils.local_to_bq_table(fname, ftype, self.project)
+    
+
+    def test_missing_schema(self):
+        """
+        Test that calling uputils.get_bq_schema for a table with missing
+        schema file causes MissingSchemaException
+        """
+        for table in self.missing_schema_tables:
+            with self.assertRaises(MissingSchemaException):
+                uputils.get_bq_schema(table)
---
+++ simeon/upload/utilities.py
@@ -0,0 +1,156 @@
+"""
+Utility functions and classes associated with uploading data to GCP, so far.
+"""
+import json
+import os
+import re
+
+from google.cloud import bigquery
+from google.cloud import storage
+
+from simeon.exceptions import (
+    BigQueryNameException, MissingSchemaException
+)
+
+
+SEGMENTS = {
+    'log': 'TRACKING-LOGS',
+    'email': 'EMAIL',
+    'sql': 'SQL',
+}
+SCHEMA_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'schemas'
+)
+SCHEMAS = {}
+
+
+def course_to_gcs_folder(course_id: str, file_type: str, bucket: str) -> str:
+    """
+    Use the given course ID to make a Google Cloud Storage path
+
+    :type course_id: str
+    :param course_id: edX course ID to format into a GCS path
+    :type file_type: str
+    :param file_type: One of sql, log, email
+    :type bucket: str
+    :param bucket: A GCS bucket name
+    :rtype: str
+    :return: A nicely formatted GCS path
+    """
+    segment = SEGMENTS.get(file_type)
+    if not segment:
+        raise ValueError('file_type is not one of these: sql, email, log')
+    if not bucket.startswith('gs://'):
+        bucket = 'gs://{b}'.format(b=bucket)
+    dirname = course_id.replace('/', '__').replace('.', '_')
+    return '{b}/{s}/{d}'.format(b=bucket, s=segment, d=dirname)
+
+
+def local_to_gcs_path(fname: str, file_type: str, bucket: str) -> str:
+    """
+    Convert the local file name into a GCS path
+
+    :type fname: str
+    :param fname: A local file name
+    :type file_type: str
+    :param file_type: One of sql, log, email
+    :type bucket: str
+    :param bucket: A GCS bucket name
+    :rtype: str
+    :return: A nicely formatted GCS path
+    """
+    segment = SEGMENTS.get(file_type)
+    if not segment:
+        raise ValueError('file_type is not one of these: sql, email, log')
+    if not bucket.startswith('gs://'):
+        bucket = 'gs://{b}'.format(b=bucket)
+    dname, bname = os.path.split(os.path.abspath(os.path.expanduser(fname)))
+    gcs_file = '{d}/{f}'.format(d=os.path.basename(dname), f=bname)
+    return '{b}/{s}/{f}'.format(b=bucket, s=segment, f=gcs_file)
+
+
+def course_to_bq_dataset(course_id: str, file_type: str, project: str) -> str:
+    """
+    Make a fully qualified BigQuery dataset name with the given info
+
+    :type course_id: str
+    :param course_id: edX course ID to format into a GCS path
+    :type file_type: str
+    :param file_type: One of sql, log, email
+    :type project: str
+    :param project: A GCP project ID
+    :rtype: str
+    :return: BigQuery dataset name with components separated by dots
+    """
+    if file_type not in ('sql', 'email', 'log'):
+        raise ValueError('file_type is not one of these: sql, email, log')
+    suffix = 'logs'
+    if file_type in ('sql', 'email'):
+        suffix = 'latest'
+    dataset = '{d}_{s}'.format(
+        d=course_id.replace('/', '__').replace('.', '_'),
+        s=suffix
+    )
+    return '{p}.{d}'.format(p=project, d=dataset)
+
+
+def local_to_bq_table(fname: str, file_type: str, project: str) -> str:
+    """
+    Use the given local file to make a fully qualified BigQuery table name
+
+    :type fname: str
+    :param fname: A local file name
+    :type file_type: str
+    :param file_type: One of sql, log, email
+    :type project: str
+    :param project: A GCP project ID
+    :rtype: str
+    :return: BigQuery dataset name with components separated by dots
+    """
+    if file_type not in ('sql', 'email', 'log'):
+        raise ValueError('file_type is not one of these: sql, email, log')
+    dname, bname = os.path.split(os.path.abspath(os.path.expanduser(fname)))
+    if file_type in ('sql', 'email'):
+        table = bname.split('.', 1)[0].replace('-', '_')
+        suffix = 'latest'
+    else:
+        suffix = 'logs'
+        table = 'tracklog_' + ''.join(re.findall(r'\d+', bname))
+    dataset = os.path.basename(dname)
+    if not table.replace('tracklog_', '') or not dataset:
+        raise BigQueryNameException(
+            'A BigQuery table name could not be constructed with {f}'.format(
+                f=fname
+            )
+        )
+    return '{p}.{d}_{s}.{t}'.format(
+        d=os.path.basename(dname), s=suffix, p=project, t=table
+    )
+
+
+def get_bq_schema(table: str, schema_dir: str=SCHEMA_DIR):
+    """
+    Given a bare table name (without leading project or dataset),
+    make a list of bigquery.SchemaField objects to act as the table's schema.
+
+    :type table: str
+    :param table: A BigQuery (bare) table name
+    :type schema_dir: str
+    :param schema_dir: Directory where schema JSON file is looked up
+    :rtype: List[bigquery.SchemaField]
+    :return: A list of bigquery.SchemaField objects
+    """
+    bname = table.split('.')[-1]
+    schema_file = os.path.join(schema_dir, 'schema_{t}.json'.format(t=bname))
+    if not os.path.exists(schema_file):
+        raise MissingSchemaException(
+            'No JSON schema file found for {t} in directory {d}'.format(
+                t=table, d=schema_dir
+            )
+        )
+    out = []
+    with open(schema_file) as jf:
+        schema = json.load(jf)
+        for field in schema.get(bname, []):
+            out.append(bigquery.SchemaField(**field))
+    return out
