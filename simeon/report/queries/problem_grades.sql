cf0bb8a5fccd48bb569130c2618b6341fc47e2ae
Jon Daries <daries@mit.edu>
2021-02-11 13:18:00 -0800

    adding sql for problem_grades

---
+++ simeon/report/queries/problem_grades.sql
@@ -0,0 +1,17 @@
+SELECT 
+    student_id as user_id,
+    module_id,
+    case 
+        when grade is not null and grade !="NULL" then CAST(grade as FLOAT64) 
+    end as grade,
+    CAST(max_grade as FLOAT64) as max_grade,
+    case 
+        when CAST(grade as FLOAT64) >= CAST(max_grade as FLOAT64) then true 
+        else false 
+    end as perfect,
+    case 
+        when grade is null or grade = "NULL" then false 
+        else true 
+    end as attempted,
+FROM `{dataset}.studentmodule`
+WHERE module_type = "problem"
