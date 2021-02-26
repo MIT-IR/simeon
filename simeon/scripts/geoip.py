6abe996d7aecf46f9d642b6c480990407009e822
Abdou Seck <aseck@mit.edu>
2021-02-26 11:02:50 -0500

    Add feature to have simeon-geoip process IP addresses inside tracking log files

--- simeon/scripts/geoip.py
+++ simeon/scripts/geoip.py
@@ -11,16 +11,16 @@ import json
 import os
 import sys
 import urllib.request as requests
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from argparse import (
+    ArgumentParser, FileType, RawDescriptionHelpFormatter
+)
+
+import simeon.scripts.utilities as cli_utils
 
 try:
     from geoip2.database import Reader as GeoReader
 except ImportError:
-    sys.exit(
-        'simeon was installed without geoip2. '
-        'please reinstall it with python -m pip install simeon[geoip]. '
-        'Or, install geoip2 with python -m pip install geoip2.'
-    )
+    GeoReader = None
 
 
 def import_un_denominations(fname=None):
@@ -56,27 +56,64 @@ def import_un_denominations(fname=None):
     return out
 
 
-def make_geo_data(fname, ip_file, outfile='geoip.json.gz', un_data=None):
+def make_geo_data(
+    db, ip_file, outfile='geoip.json.gz',
+    un_data=None, tracking_logs=False, logger=None,
+):
     """
     Given a MaxMind DB and a credentials file to PROPROD,
     extract distinct IPs from obscured and get their location data
+
+    :type fname: geoip2.database.Reader
+    :param fname: MaxMind geolocation database Reader object to extract info
+    :type ip_file: str
+    :param ip_file: A file (text or tracking log) containing IP addresses
+    :type outfile: str
+    :param outfile: File in which to dump geolocation data
+    :type un_data: Union[Dict[str, str], None]
+    :param un_data: Dictionary containing UN country denomination information
+    :type tracking_logs: bool
+    :param tracking_logs: Whether or not the given IP file is a tracking log
+    :type logger: Union[logging.Logger, None]
+    :param logger: A logger object to log messages to specific streams
+    :rtype: None
+    :return: Writes data to the given output file in append mode
     """
-    try:
-        un_denoms = import_un_denominations(un_data)
-    except Exception as e:
-        msg = 'Failed to get UN denominations because: {e}'
-        print(msg.format(e=e), file=sys.stderr)
-        un_denoms = {}
-    locs = GeoReader(fname)
-    with open(ip_file) as fh, open(outfile, 'wt') as outh:
-        reader = csv.reader(fh)
-        for rec in reader:
+    if tracking_logs:
+        fh = gzip.open(ip_file, 'rt')
+        reader = map(json.loads, fh)
+    else:
+        fh = open(ip_file)
+        reader = csv.DictReader(fh, fieldnames=['ip'])
+    with gzip.open(outfile, 'at') as outh:
+        seen = set()
+        line = 0
+        while True:
+            line += 1
             try:
-                info = locs.city(rec[0])
-                un_info = un_denoms.get(info.country.iso_code, {})
+                rec = next(reader)
+            except StopIteration:
+                break
+            except Exception as excp:
+                msg = (
+                    'Record {i} from {f} could not be parsed: {e}'
+                    '\nSkipping it...'
+                ).format(i=line, f=ip_file, e=excp)
+                if logger:
+                    logger.warn(msg)
+                else:
+                    print(msg, file=sys.stderr)
+                continue
+            ip_address = rec.get('ip')
+            if not ip_address or ip_address in seen:
+                continue
+            seen.add(ip_address)
+            try:
+                info = db.city(ip_address)
+                un_info = un_data.get(info.country.iso_code, {})
                 subdiv = info.subdivisions.most_specific
                 row = {
-                    'ip': rec[0],
+                    'ip': ip_address,
                     'city': info.city.names.get('en'),
                     'countryLabel': info.country.names.get('en'),
                     'country': info.country.iso_code,
@@ -91,9 +128,10 @@ def make_geo_data(fname, ip_file, outfile='geoip.json.gz', un_data=None):
                 row.update(un_info)
             except Exception:
                 row = {
-                    'ip': rec[0],
+                    'ip': ip_address,
                 }
             outh.write(json.dumps(row) + '\n')
+    fh.close()
 
 
 def main():
@@ -104,10 +142,31 @@ def main():
         description=__doc__,
         formatter_class=RawDescriptionHelpFormatter,
     )
-    parser.add_argument('db', help='MaxMind Geo DB')
     parser.add_argument(
-        'ip_file',
-        help='A file with IP addresses. One IP address per line',
+        'db',
+        help='MaxMind version 2 geolocation database file.'
+    )
+    parser.add_argument(
+        'ip_files',
+        help=(
+            'File(s) containing IP addresses. If it\'s a text file, '
+            'it should be one IP address per record.\nIf the file is '
+            'a tracking log file, then each record is assumed to have '
+            'an "ip" string value.'
+        ),
+        nargs='+',
+    )
+    parser.add_argument(
+        '--log-file', '-L',
+        help='Log file to use when simeon prints messages. Default: stdout',
+        type=FileType('w'),
+        default=sys.stdout,
+    )
+    parser.add_argument(
+        '--quiet', '-Q',
+        help='Only print error messages to standard streams.',
+        action='store_false',
+        dest='verbose',
     )
     parser.add_argument(
         '--un-data', '-u',
@@ -122,11 +181,57 @@ def main():
     parser.add_argument(
         '--output', '-o',
         help='Output file name for the generated data. Default: %(default)s',
-        default=os.path.join(os.getcwd(), 'geoip.json.gz')
+        default=os.path.join(os.getcwd(), 'geoip.json.gz'),
+    )
+    parser.add_argument(
+        '--tracking-logs', '-t',
+        help='Whether or not the given ip files are tracking log files',
+        action='store_true',
     )
     args = parser.parse_args()
-    make_geo_data(args.db, args.ip_file, args.output, args.un_data)
-
+    args.logger = cli_utils.make_logger(
+        user='SIMEON:GEOIP',
+        verbose=args.verbose,
+        stream=args.log_file,
+    )
+    if not GeoReader:
+        args.logger.error(
+            'simeon was installed without geoip2. '
+            'please reinstall it with python -m pip install simeon[geoip]. '
+            'Or, install geoip2 with python -m pip install geoip2.'
+        )
+        sys.exit(1)
+    try:
+        os.remove(args.output)
+    except OSError:
+        pass
+    try:
+        un_denoms = import_un_denominations(args.un_data)
+    except Exception as e:
+        msg = 'Failed to get UN denominations because: {e}'
+        if args.logger:
+            args.logger.warn(msg.format(e=e))
+        else:
+            print(msg.format(e=e), file=sys.stderr)
+        un_denoms = {}
+    locs = GeoReader(args.db)
+    for ip_file in args.ip_files:
+        args.logger.info(
+            'Processing IP addresses in {f} with geolocation DB {db}'.format(
+                f=ip_file, db=args.db
+            )
+        )
+        make_geo_data(
+            db=locs, ip_file=ip_file,
+            outfile=args.output, un_data=un_denoms,
+            tracking_logs=args.tracking_logs,
+            logger=args.logger,
+        )
+        args.logger.info(
+            'Done with IP addresses in {f}'.format(
+                f=ip_file,
+            )
+        )
 
 if __name__ == '__main__':
     main()
