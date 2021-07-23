f70122dc4c3ad04a82c35eda9a14d81f5e7b9a54
Abdou Seck <aseck@mit.edu>
2021-07-23 17:27:05 -0400

    REPORT: Change the definition of pc_nchapters to catch chapters that don't show up in studentmodule

--- simeon/report/queries/pc_nchapters.sql
+++ simeon/report/queries/pc_nchapters.sql
@@ -1,14 +1,9 @@
-select 
-    user_id, 
-    count(*) as nchapters 
-from (
-        SELECT 
-            student_id as user_id, 
-            module_id, 
-            count(*) as chapter_views
-        FROM `{latest_dataset}.studentmodule`
-        where module_type = "chapter"
-        group by user_id, module_id
-    )
-group by user_id
-order by user_id
+select
+    student_id as user_id,
+    count(distinct chapter_mid) as nchapters
+from `{latest_dataset}.studentmodule`
+join `{latest_dataset}.course_axis`
+using(module_id)
+where chapter_mid is not null
+group by 1
+order by 1
