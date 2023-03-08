a5720efb07c0077ade5165697d55f90dc128fc4c
Abdou Seck <aseck@mit.edu>
2023-03-08 11:20:47 -0500

    using safe_cast() to account for blank strings in studentmodule

--- simeon/report/queries/problem_grades.sql
+++ simeon/report/queries/problem_grades.sql
@@ -1,16 +1,14 @@
 SELECT 
     student_id as user_id,
     module_id,
+    SAFE_CAST(grade as FLOAT64) as grade,
+    SAFE_CAST(max_grade as FLOAT64) as max_grade,
     case 
-        when grade is not null and grade !="NULL" then CAST(grade as FLOAT64) 
-    end as grade,
-    CAST(max_grade as FLOAT64) as max_grade,
-    case 
-        when CAST(grade as FLOAT64) >= CAST(max_grade as FLOAT64) then true 
+        when SAFE_CAST(grade as FLOAT64) >= SAFE_CAST(max_grade as FLOAT64) then true 
         else false 
     end as perfect,
     case 
-        when grade is null or grade = "NULL" then false 
+        when grade is null or grade = "NULL" or grade = "" then false 
         else true 
     end as attempted,
 FROM `{latest_dataset}.studentmodule`
